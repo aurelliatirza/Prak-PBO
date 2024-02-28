f45f598761172781f9b38c8eab2c6c131bdf2bb5
aurelliatirza <126539060+aurelliatirza@users.noreply.github.com>
2024-02-28 23:58:03 +0700

    MTitik.java

--- src/MTitik.java
+++ src/MTitik.java
@@ -1,22 +1,3 @@
-// public class MTitik {
-    
-//     public static void main(String[] args) {
-//         Titik T1 = new Titik();
-//         Titik T2 = new Titik();
-//         //Titik T3;
-
-//         T1.setAbsis(1);
-//         T1.setOrdinat(1);
-//         T2.setAbsis(4);
-//         T2.setOrdinat(5);
-
-        
-//         // System.out.println(("\nJumkah Objek titik: " + T2.getCounter()));
-//         System.out.println("T1(" + T1.getAbsis()+ ", " + T1.getOrdinat() + ")");
-//         System.out.println("T2(" + T2.getAbsis()+ ", " + T2.getOrdinat() + ")");
-//         // System.out.println("\nT3(" + T3.getAbsis()+ ", " + T3.getOrdinat() + ")");       
-//     }
-// }
 
 
 class MTitik
